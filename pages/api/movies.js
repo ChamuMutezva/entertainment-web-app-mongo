4fa42ca95ab127d759c615c28625f0840e0b459d
Chamu Mutezva <ckmutezva@gmail.com>
2023-10-11 02:31:54 +0200

    initial start project

---
+++ pages/api/movies.js
@@ -0,0 +1,18 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async  (req, res) => {
+   try {
+       const client = await clientPromise;
+       const db = client.db("movies_data");
+
+       const movies = await db
+           .collection("movies")
+           .find({})
+           .sort({ metacritic: -1 })           
+           .toArray();
+
+       res.json(movies);
+   } catch (e) {
+       console.error(e);
+   }
+};
