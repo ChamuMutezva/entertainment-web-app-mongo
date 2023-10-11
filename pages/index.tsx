c743eb2579f2d9f15a7175523f1c65311b11a4f2
Chamu Mutezva <ckmutezva@gmail.com>
2023-10-11 18:51:19 +0200

    using static generation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,37 @@
-import Head from "next/head";
 import clientPromise from "../lib/mongodb";
-import type { InferGetServerSidePropsType } from "next";
 
-type ConnectionStatus = {
-    isConnected: boolean;
-};
+interface Movie {
+    title: string;
+    year: string;
+    category: string;
+}
 
-export async function getServerSideProps() {
+interface TopProps {
+    movies: Movie[];
+}
+
+export default function Top({ movies }: TopProps) {
+    return (
+        <div>
+            <h1>Next.js static generation with MongoDB</h1>
+            <ul>
+                {movies.map((movie) => (
+                    <li>
+                        <h2>{movie.title}</h2>
+                        <h3>{movie.category}</h3>
+                        <p>{movie.year}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export async function getStaticProps() {
     try {
-        await clientPromise;
-        // `await clientPromise` will use the default database passed in the MONGODB_URI
-        // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-        //
         const client = await clientPromise;
         const db = client.db("movies_data");
-        //
-        // Then you can execute queries against your database like so:
-        // db.find({}) or any of the MongoDB Node Driver commands
+
         const movies = await db
             .collection("movies")
             .find({})
@@ -24,47 +39,9 @@ export async function getServerSideProps() {
             .toArray();
 
         return {
-            props: {
-                movies: JSON.parse(JSON.stringify(movies)),
-                isConnected: true,
-            },
+            props: { movies: JSON.parse(JSON.stringify(movies)) },
         };
     } catch (e) {
         console.error(e);
     }
-}
-
-export default function Home({
-    isConnected,
-    movies,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-    return (
-        <div className="container">
-            <Head>
-                <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <main>
-                <div>
-                    <h1> Movies 2023</h1>
-                    <ul>
-                        {movies?.map(
-                            (movie: {
-                                title: string;
-                                year: string;
-                                category: string;
-                            }) => (
-                                <li key={movie.title}>
-                                    <h2>{movie.title}</h2>
-                                    <h3>{movie.year}</h3>
-                                    <p>{movie.category}</p>
-                                </li>
-                            )
-                        )}
-                    </ul>
-                </div>
-            </main>
-        </div>
-    );
 }
