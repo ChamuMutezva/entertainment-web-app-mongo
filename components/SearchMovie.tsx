e837ed7372cb14c162a4d499ca49012738c24806
Chamu Mutezva <ckmutezva@gmail.com>
2023-10-18 05:47:15 +0200

    feat: form search component

--- components/SearchMovie.tsx
+++ components/SearchMovie.tsx
@@ -1,25 +1,35 @@
 import React from "react";
 import Image from "next/image";
 
-function SearchMovie() {
+function SearchMovie(props: {
+    searchMovie: React.ChangeEventHandler<HTMLInputElement>;
+    searchText: string;
+}) {
     return (
         <form noValidate className="relative py-4 bg-inherit">
             <input
                 type="search"
                 name="search-movie"
                 id="search-movie"
-                placeholder=" "
-                className="w-full text-darkBlue bg-darkBlue focus:ring-0 peer"
+                placeholder=""
+                className="w-full text-white bg-darkBlue focus:ring-0 peer"
+                onChange={props.searchMovie}
+                value={props.searchText}
             />
+            {/*  absolute -top-3 left-2
+                       transition-all bg-white scale-75 
+                       px-1 duration 
+                       peer-placeholder-shown:scale-100
+                        peer-placeholder-shown:top-2.5 
+                        peer-placeholder-shown:left-2
+                         peer-placeholder-shown:text-slate-500 
+                         peer-focus:-top-3 peer-focus:scale-75
+                          peer-focus:-left-2 peer-focus:text-sky-500
+                       */}
             <label
                 htmlFor="search-movie"
-                className="absolute flex items-center gap-2 text-greyishBlue top-6 left-2 transition-all scale-75 px-1 duration-300
-                            peer-placeholder-shown:scale-100 
-                            peer-placeholder-shown:top-4.5
-                            peer-placeholder-shown:left-2 
-                            peer-placeholder-shown:text-slate:500 
-                            peer-focus:-top-3 peer-focus:scale-75
-                            peer-focus:-left-2 peer-focus:text-white"
+                className="absolute flex items-center gap-2
+                             text-greyishBlue top-6 left-2 transition-all "
             >
                 <Image
                     src={"/assets/icon-search.svg"}
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import Head from "next/head";
 import Navigation from "../components/navigation";
 import Trending from "../components/Trending";
 import Recommended from "../components/Recommended";
+import SearchMovie from "../components/SearchMovie";
 
 type ConnectionStatus = {
     isConnected: boolean;
@@ -83,40 +84,10 @@ export default function Home({
             <main className="bg-darkBlue text-white">
                 <div className="py-6">
                     <h1 className="text-red sr-only">Entertainment center</h1>
-                    <form noValidate className="relative py-4 bg-inherit">
-                        <input
-                            type="search"
-                            name="search-movie"
-                            id="search-movie"
-                            placeholder=""
-                            className="w-full text-white bg-darkBlue focus:ring-0 peer"
-                            onChange={searchMovie}
-                            value={searchText}
-                        />
-                        {/*  absolute -top-3 left-2
-                       transition-all bg-white scale-75 
-                       px-1 duration 
-                       peer-placeholder-shown:scale-100
-                        peer-placeholder-shown:top-2.5 
-                        peer-placeholder-shown:left-2
-                         peer-placeholder-shown:text-slate-500 
-                         peer-focus:-top-3 peer-focus:scale-75
-                          peer-focus:-left-2 peer-focus:text-sky-500
-                       */}
-                        <label
-                            htmlFor="search-movie"
-                            className="absolute flex items-center gap-2
-                             text-greyishBlue top-6 left-2 transition-all "
-                        >
-                            <Image
-                                src={"/assets/icon-search.svg"}
-                                width={32}
-                                height={32}
-                                alt=""
-                            />
-                            Search for movies or Tv series
-                        </label>
-                    </form>
+                    <SearchMovie
+                        searchMovie={searchMovie}
+                        searchText={searchText}
+                    />
                     <Trending movies={movies} />
                     <Recommended movies={movies} />
                 </div>
